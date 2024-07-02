3dae01fb49594fac5f714d087bed6feb5b731cd8
Inaara123 <tech@inaara.ai>
2024-07-02 08:53:19 -0700

    fixed some ui

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
   };
 
   const style = {
-    backgroundColor: 'rgba(83, 88, 111, 0.27)', // Background color with 27% opacity
+    backgroundColor: 'rgba(83, 88, 111, 0.27)', 
   };
 
   return (
@@ -38,18 +38,18 @@ const Footer = () => {
           KS bakery, back side, Madhava Nagar Colony, Ambedkar Nagar, Hafeezpet, Hyderabad, Telangana 500049
         </p>
 
-        <p className='mt-4 text-[20px] text-custom-blue xsm:text-[18px] lg:ml-[-600px] xsm:ml-[10px] flex flex-row gap-[10px] sm:ml-[100px] md:ml-[100px] sm:mr-[100px] md:mr-[100px] ml-[100px] mr-[100px]'>
+        <p className='mt-4 text-[20px] text-custom-blue xsm:text-[18px] lg:ml-[-500px] xsm:ml-[10px] flex flex-row gap-[10px] sm:ml-[100px] md:ml-[100px] sm:mr-[100px] md:mr-[100px] ml-[100px] mr-[100px]'>
           <FaPhoneAlt className='mt-[8px]' />
           +91-9192939495
         </p>
-        <p className='mt-8 text-[20px] text-custom-blue xsm:text-[18px] lg:ml-[-470px] xsm:ml-[10px] flex flex-row gap-[20px] ml-[100px] sm:ml-[100px] md:ml-[100px] sm:mr-[100px] md:mr-[100px] mr-[100px]'>
+        <p className='mt-8 text-[20px] text-custom-blue xsm:text-[18px] lg:ml-[-380px] xsm:ml-[10px] flex flex-row gap-[20px] ml-[100px] sm:ml-[100px] md:ml-[100px] sm:mr-[100px] md:mr-[100px] mr-[100px]'>
           <MdEmail className='mt-[10px]' />
           resetwellnesscenter@gmail.com
         </p>
-        <p className='font-outfit font-medium text-[32px] mt-8 xsm:ml-[10px] lg:ml-[-650px] sm:ml-[100px] md:ml-[100px] sm:mr-[100px] md:mr-[100px] ml-[100px] mr-[100px]' style={{ color: "#152344" }}>
+        <p className='font-outfit font-medium text-[32px] mt-8 xsm:ml-[10px] lg:ml-[-540px] sm:ml-[100px] md:ml-[100px] sm:mr-[100px] md:mr-[100px] ml-[100px] mr-[100px]' style={{ color: "#152344" }}>
           Follow us
         </p>
-        <div className='flex text-custom-blue flex-row gap-8 mt-4 lg:mt-8 xsm:ml-[10px] lg:ml-[-520px] sm:ml-[100px] md:ml-[100px] sm:mr-[100px] md:mr-[100px] ml-[100px] mr-[100px]'>
+        <div className='flex text-custom-blue flex-row gap-8 mt-4 lg:mt-8 xsm:ml-[10px] lg:ml-[-500px] sm:ml-[100px] md:ml-[100px] sm:mr-[100px] md:mr-[100px] ml-[100px] mr-[100px]'>
           <a href={facebookUrl} target="_blank" rel="noopener noreferrer" onClick={handleFacebookClick}>
             <FaFacebookSquare size={40} />
           </a>
@@ -61,8 +61,8 @@ const Footer = () => {
           </a>
         </div>
         <div className='relative mt-8 lg:mt-16 w-full flex justify-center'>
-          <div className='h-[280px] w-[280px] rounded-[20px] hidden xl:block 2xl:block lg:mt-[-430px] lg:ml-[330px]' style={{ backgroundColor: "#DBFFEB" }}></div>
-          <img src={locationImg} alt="Location" height={183} width={183} className='hidden xl:block 2xl:block absolute top-16 lg:mt-[-440px] lg:ml-[330px]' />
+          <div className='h-[280px] w-[280px] rounded-[20px] hidden xl:block 2xl:block lg:mt-[-430px] lg:ml-[280px]' style={{ backgroundColor: "#DBFFEB" }}></div>
+          <img src={locationImg} alt="Location" height={183} width={183} className='hidden xl:block 2xl:block absolute top-16 lg:mt-[-440px] lg:ml-[280px]' />
         </div>
       </div>
     </main>
